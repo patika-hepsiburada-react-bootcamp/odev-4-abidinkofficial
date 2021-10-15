d77e54e13242f99f0a980a145e50758c7c52279c
Abidin K <abidinkofficial@gmail.com>
2021-10-15 11:24:32 +0300

    Added langugage & metrics config to the query

--- src/api/queries.js
+++ src/api/queries.js
@@ -1,8 +1,8 @@
 import { gql } from "@apollo/client";
 
 export const GET_CITY_BY_NAME = gql`
-  query getCityByName($name: String!) {
-    getCityByName(name: $name) {
+  query getCityByName($name: String!, $country: String, $config: ConfigInput) {
+    getCityByName(name: $name, country: $country, config: $config) {
       name
       weather {
         summary {
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,15 @@
+import { useQuery, useLazyQuery } from "@apollo/client"
+import { GET_CITY_BY_NAME } from "../../api/queries"
+
 import Search from "../../components/Search"
 import WeatherCard from "../../components/WeatherCard"
 
 const Home = () => {
+  const { loading, data, error } = useQuery(GET_CITY_BY_NAME, {
+    variables: { name: "corum", country: "TR", config: {units: "metric", lang: "tr"} }
+  })
+  console.log(data)
+
   return (
     <div>
       Home
