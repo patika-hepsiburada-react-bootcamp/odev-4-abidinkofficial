b8349fe57afe094ff6580984a165dd1e8726633f
Abidin K <abidinkofficial@gmail.com>
2021-10-17 23:06:25 +0300

    Added basic styling to the cities dropdown

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,8 +1,9 @@
+import styles from "./Search.module.css"
 import cities from "../../utils/cities"
 
 const Search = ({ city, setCity }) => {
   return (
-    <div>
+    <div className={styles["Search"]}>
       <select value={city} onChange={(e) => setCity(e.target.value)}>
         {
           cities.map(city => <option key={city.id} value={city.name}>{city.city}</option>)
--- src/components/WeatherCard/index.js
+++ src/components/WeatherCard/index.js
@@ -15,15 +15,13 @@ const WeatherCard = ({ city }) => {
           src={`https://openweathermap.org/img/wn/${data.getCityByName.weather.summary.icon}@4x.png`}
           alt={`${data.getCityByName.weather.summary.title}`}
         />
+        <h2>{data.getCityByName.weather.temperature.actual} &#176;C</h2>
       </div>
       <div className={styles["card-temp"]}>
-        <h2>{data.getCityByName.weather.temperature.actual} &#176;C</h2>
         <div className={styles["card-misc"]}>
           <div>Hissedilen <span>{data.getCityByName.weather.temperature.feelsLike} &#176;C</span></div>
           <div>En yüksek <span>{data.getCityByName.weather.temperature.max} &#176;C</span></div>
           <div>En düşük <span>{data.getCityByName.weather.temperature.min} &#176;C</span></div>
-        </div>
-        <div className={styles["card-misc"]}>
           <div>Rüzgar <span>{data.getCityByName.weather.wind.speed} km/h</span></div>
           <div>Nem <span>%{data.getCityByName.weather.clouds.humidity}</span></div>
         </div>
